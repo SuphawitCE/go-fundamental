c7c8c6975cf14e2fbda01bddfca7d63fad112be4
SuphawitCE <b4nknaja@hotmail.com>
2023-07-30 18:20:56 +0700

    feat(fetch): [GO-000] fetching products

--- src/random/cart/input/db.go
+++ src/random/cart/input/db.go
@@ -1,7 +1,40 @@
 package input
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-func GetCart() {
+type Product struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Price       float64 `json:"price"`
+	Description string  `json:"description"`
+	Category    string  `json:"category"`
+	Image       string  `json:"image"`
+}
+
+func GetCart() []Product {
 	fmt.Println("GetCart")
+	url := "https://fakestoreapi.com/products"
+
+	// Send GET request
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Get product error", err)
+		// return
+	}
+
+	defer response.Body.Close()
+
+	// Parse JSON response
+	var products []Product
+	err = json.NewDecoder(response.Body).Decode(&products)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		// return
+	}
+	return products
 }
