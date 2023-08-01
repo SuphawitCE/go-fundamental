8b7b5dd30b1aaf6e3340dce2895c9bbc427cb8a6
SuphawitCE <b4nknaja@hotmail.com>
2023-08-01 16:46:28 +0700

    feat(random): [GO-000] get input from kb

--- src/random/cart/input/kb.go
+++ src/random/cart/input/kb.go
@@ -5,7 +5,7 @@ import (
 	"log"
 )
 
-func ReadInputFromKeyBoard() []int {
+func ReadInputFromKeyBoard() ([]int, []int) {
 	fmt.Print("Number of product in cart: ")
 	var product int
 	_, err := fmt.Scan(&product)
@@ -15,14 +15,17 @@ func ReadInputFromKeyBoard() []int {
 	}
 
 	var productIds = make([]int, product)
+	var productQuantity = make([]int, product)
 
 	for i := 0; i < len(productIds); i++ {
-		_, err := fmt.Scan(&productIds[i])
+		_, err := fmt.Scan(&productIds[i], &productQuantity[i])
 
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	return productIds
+	fmt.Println("input:", productIds, productQuantity)
+
+	return productIds, productQuantity
 }
